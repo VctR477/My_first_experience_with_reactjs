b827caafdeb8ed490b8472dd1011701d81600270
vklimov <vklimov@livemaster.ru>
2018-09-23 12:52:09 +0300

    Add first component Article
    Signed-off-by: vklimov <vklimov@livemaster.ru>

---
+++ src/Article.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class Article extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isOpen: false
+		};
+	}
+
+	render() {
+		const { article } = this.props;
+		const { isOpen } = this.state;
+		const section = isOpen ? <section>{article.text}</section> : null;
+		return (
+			<div>
+				<h3>{article.title}</h3>
+				<button onClick={this.toogleOpen}>
+					{ isOpen ? 'close' : 'open' }
+				</button>
+				{ section }
+			</div>
+		);
+	}
+	
+	toogleOpen = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+};
